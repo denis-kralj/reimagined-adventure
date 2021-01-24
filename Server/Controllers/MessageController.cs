1fdd01cb77c83c038f6266f7ab54ecd2d31cc271
Denis Kralj <kralj.denis.zg@gmail.com>
2021-01-24 17:00:22 +0100

    Fixes #19
    Add new message controller to server
    Add call to new controller action on chat initialization

---
+++ Server/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ReimaginedAdventure.Shared;
+
+namespace ReimaginedAdventure.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private IChatStore _chatStore;
+
+        public MessageController(IChatStore chatStore)
+        {
+            _chatStore = chatStore;
+        }
+        [HttpGet]
+        public IEnumerable<ChatMessage> Get()
+        {
+            return _chatStore.GetAllMessages().ToArray();
+        }
+    }
+}

a0ea52b5ae7a3ebe488b0848887d1cd1a5716bd5
Denis Kralj <kralj.denis.zg@gmail.com>
2021-02-14 14:10:16 +0100

    Fixes #41
    Adjust chat input to work with null user
    Add Id prop to user object
    Remove junk left over from experiments
    Add user data validity check to landing and user data edit form

--- Shared/User.cs
+++ Shared/User.cs
@@ -1,9 +1,11 @@
+using System;
 using SD = System.Drawing;
 
 namespace ReimaginedAdventure.Shared
 {
     public record User
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Handle { get; set; } = string.Empty;
         public string Color { get; set; } = SD.Color.Black.ToHexCode();
     }

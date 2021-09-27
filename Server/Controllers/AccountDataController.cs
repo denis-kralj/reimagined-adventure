ea19e82fb8fa442832c9d7014421f61034e79f92
Denis Kralj <kralj.denis.zg@gmail.com>
2021-09-27 20:42:54 +0200

    Fixes #59
    Add user bar component
    Set index to dedicated index content
    Add labels to login and register forms
    Move towards null as error idea
    Add wrapper to help with page building
    Add new component to main layout
    Adjust grid definitions in css
    Add needed model and controller

---
+++ Server/Controllers/AccountDataController.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReimaginedAdventure.Shared.Models;
+
+namespace ReimaginedAdventure.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountDataController : ControllerBase
+    {
+        private AccountManager _accountManager;
+
+        public AccountDataController(AccountManager accountManager)
+        {
+            _accountManager = accountManager;
+        }
+
+        [HttpGet]
+        public Task<AccountDataModel> Post()
+        {
+
+            return Task.FromResult(new AccountDataModel
+            {
+                IsAuthenticated = this.User.Identity.IsAuthenticated,
+                Email = this.User.Identity.Name
+            });
+        }
+    }
+}
